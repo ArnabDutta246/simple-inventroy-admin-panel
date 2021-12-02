a8a745d4c13fea4faf15fecf5b2264bb1a5187d2
ArnabDutta246 <arnab2461997@gmail.com>
2021-12-02 23:39:11 +0530

    add new

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -4,8 +4,12 @@ import { AboutComponent } from './about.component';
 import { HeaderModule } from 'src/app/shared-component/header/header.module';
 import { BreadcrumbModule } from 'src/app/shared-component/breadcrumb/breadcrumb.module';
 import { FooterModule } from 'src/app/shared-component/footer/footer.module';
+import { RouterModule, Routes } from '@angular/router';
+import { CarouselModuleExport } from 'src/app/shared-component/carousel/carousel.module';
 
-
+const routes:Routes = [
+  {path:'',component:AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +19,9 @@ import { FooterModule } from 'src/app/shared-component/footer/footer.module';
     CommonModule,
     HeaderModule,
     BreadcrumbModule,
-    FooterModule
+    FooterModule,
+    RouterModule.forChild(routes),
+    CarouselModuleExport
   ],
-  exports:[
-    AboutComponent
-  ]
 })
 export class AboutModule { }
