8cea2a566640d96574e7d0f7e8b015d8c3a1cd3d
Arnab Dutta <arnab2461997@gmail.com>
2022-01-09 15:09:50 +0530

    server add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,21 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDzlqg6gOkKAvVUnIcNglM0afsJFiOntVk",
-    authDomain: "application-testing-ad7df.firebaseapp.com",
-    projectId: "application-testing-ad7df",
-    storageBucket: "application-testing-ad7df.appspot.com",
-    messagingSenderId: "354898241858",
-    appId: "1:354898241858:web:08b01696de66ceb84780db",
-    measurementId: "G-8EETRBVN0Q"
-  }
+    // apiKey: "AIzaSyDzlqg6gOkKAvVUnIcNglM0afsJFiOntVk",
+    // authDomain: "application-testing-ad7df.firebaseapp.com",
+    // projectId: "application-testing-ad7df",
+    // storageBucket: "application-testing-ad7df.appspot.com",
+    // messagingSenderId: "354898241858",
+    // appId: "1:354898241858:web:08b01696de66ceb84780db",
+    // measurementId: "G-8EETRBVN0Q"
+
+    //==============[ Main ]================
+    apiKey: 'AIzaSyDk380dUo1fP3L8k6hEld76TkLtOx4WSCA',
+    authDomain: 'howladerelectronics-6ce3d.firebaseapp.com',
+    projectId: 'howladerelectronics-6ce3d',
+    storageBucket: 'howladerelectronics-6ce3d.appspot.com',
+    messagingSenderId: '397320909791',
+    appId: '1:397320909791:web:fb04fee331f9a22b808a6a',
+    measurementId: 'G-7HCCFT021Z',
+  },
 };
